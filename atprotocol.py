8dc5a9bc39231381ad79dfe39893b6a82155b387
Jesse Fuller <jeshii@gmail.com>
2025-01-20 22:20:17 +0900

    Some more stuff for tomorrow

--- atprotocol.py
+++ atprotocol.py
@@ -11,27 +11,40 @@ import logging
 import sys
 import termios
 import tty
+import piexif
+
 
 def get_password_from_keyring(logger, user: str):
-  try:
-    password = keyring.get_password("photomise-atprotocol", user)
-    return password
-  except Exception as e:
-    logger.debug(f"Unable to get password: {e}")
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
     try:
-        tty.setraw(sys.stdin.fileno())
-        password = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    keyring.set_password("photomise-atprotocol", user, password)
-    return password
+        password = keyring.get_password("photomise-atprotocol", user)
+        return password
+    except Exception as e:
+        logger.debug(f"Unable to get password: {e}")
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            password = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        keyring.set_password("photomise-atprotocol", user, password)
+        return password
+
+
+def get_image_aspect_ratio(image_path: str) -> tuple:
+    try:
+        exif_dict = piexif.load(image_path)
+        image_width = exif_dict["0th"][piexif.ImageIFD.ImageWidth]
+        image_height = exif_dict["0th"][piexif.ImageIFD.ImageLength]
+        return (image_width, image_height)
+    except (piexif.InvalidImageDataError, KeyError):
+        return None
+
 
 def main(args):
     client = Client()
     logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(__name__) 
+    logger = logging.getLogger(__name__)
 
     if not args.user:
         args.user = inquirer.text(message="Enter username").execute()
@@ -39,11 +52,11 @@ def main(args):
     password = get_password_from_keyring(logger, args.user)
     client.login(args.user, password)
 
-    paths = ['cat.jpg', 'dog.jpg', 'bird.jpg']
+    paths = ["cat.jpg", "dog.jpg", "bird.jpg"]
     image_alts = [
-        'Text version',
-        'of the image (ALT)',
-        'This parameter is optional',
+        "Text version",
+        "of the image (ALT)",
+        "This parameter is optional",
     ]
 
     image_aspect_ratios = [
@@ -54,17 +67,18 @@ def main(args):
 
     images = []
     for path in paths:
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             images.append(f.read())
 
     client.send_images(
-        text='Post with image from Python',
+        text="Post with image from Python",
         images=images,
         image_alts=image_alts,
         image_aspect_ratios=image_aspect_ratios,
     )
 
     return
+
 
 def parse_args():
     description = "Upload photos processed by photomise to bluesky"
